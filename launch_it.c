5575fe34771e5d85b88557e0fb6f6417e74f0e10
theinerm <theiner@sosehl.cz>
2026-02-17 13:23:19 +0100

    Vytvoren soubor launch_it.c

---
+++ launch_it.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+
+/* Jednoduchá hashovací funkce (není triviální na pohled) */
+unsigned long simple_hash(const char *s) {
+    unsigned long h = 5381;
+    int c;
+
+    while ((c = *s++))
+        h = ((h << 5) + h) ^ c;
+
+    return h;
+}
+
+int main(void) {
+    const char *user = getlogin();
+    if (!user) {
+        fprintf(stderr, "Nelze zjistit uživatele.\n");
+        return 1;
+    }
+
+    char hostname[256];
+    if (gethostname(hostname, sizeof(hostname)) != 0) {
+        perror("gethostname");
+        return 1;
+    }
+
+    uid_t uid = getuid();
+
+    /* spojení údajů */
+    char buffer[512];
+    snprintf(buffer, sizeof(buffer), "%s:%s:%d", user, hostname, uid);
+
+    unsigned long hash = simple_hash(buffer);
+
+    /* název souboru */
+    char path[512];
+    snprintf(path, sizeof(path),
+             "%s/.task_%lx.dat",
+             getenv("HOME"),
+             hash);
+
+    FILE *f = fopen(path, "w");
+    if (!f) {
+        perror("fopen");
+        return 1;
+    }
+
+    fprintf(f,
+        "Student: %s\n"
+        "Host: %s\n"
+        "UID: %d\n"
+        "Hash: %lx\n",
+        user, hostname, uid, hash);
+
+    fclose(f);
+
+    printf("Hotovo. Soubor byl vytvoren.\n");
+    return 0;
+}
